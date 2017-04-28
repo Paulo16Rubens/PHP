1b15f6c10a547dd50869b464ba09f94aa0265842
Paulo16Rubens <prubens42@gmail.com>
2017-04-28 15:51:55 -0300

    Create Exemplo1.php

---
+++ Exemplo1.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta charset="utf-8" />
+
+</head>
+<body>
+<?php
+$idade=16;
+$nome ="Paulo Rubens";
+echo "OlÃ¡ meu nome ",$nome,"<br>Minha idade Ã© ",$idade;
+?>
+</body>
+</html
