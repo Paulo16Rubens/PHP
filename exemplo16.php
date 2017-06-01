f45c9f2a20d5d1a7c877e55716c4109b99d7b59d
Paulo16Rubens <prubens42@gmail.com>
2017-06-01 01:52:08 +0100

    Create exemplo16.php

---
+++ exemplo16.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<h1>Exemplo 16</h1>
+<?php
+function teste($txt = "Testando"){
+echo"<h2> $txt </h2>";
+}
+teste();
+teste("Outro Teste");
+?>
+</body>
+</html>
