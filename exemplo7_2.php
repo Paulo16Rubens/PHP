84aa22c46bca4c102f22bbc20e0d99478fc0bfd6
Paulo16Rubens <prubens42@gmail.com>
2017-06-01 01:39:36 +0100

    Create exemplo7_2.php

---
+++ exemplo7_2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<h1>Estrutura Condicional - If/Else)</h1>
+<?php
+$idade=12;
+$maioridade=18;
+if($idade>=$maioridade){
+echo("<br><b>"+$idade."anos, você já é maior de idade.</b>");
+}
+else{
+echo("<br><b>"+$idade."anos, você ainda não é maior de idade.</b>");
+}
+?>
+</body>
+</html>
