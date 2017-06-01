17a350b3fc945e8709731f5842b908e6432773ac
Paulo16Rubens <prubens42@gmail.com>
2017-06-01 01:43:38 +0100

    Create exemplo10.php

---
+++ exemplo10.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<h1>Estrutura de Repetição - While</h1>
+<?php
+$num=1;
+while($num<=10){
+echo"Número: $num <br>";
+$num++;
+}
+?>
+</body>
+</html>
