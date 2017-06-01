3bf017d0c93eed14a61daf5b2bf776338e639439
Paulo16Rubens <prubens42@gmail.com>
2017-06-01 01:46:57 +0100

    Create exemplo13.php

---
+++ exemplo13.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<?php
+function soma($n1,$n2){
+return $n1+$n2;
+} function imprimir(){
+return"O Resultado da Soma é:";
+}
+echo imprimir().soma(115,30);
+?>
+</body>
+</html>
