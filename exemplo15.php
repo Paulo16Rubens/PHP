9a8a855092b1a2fe9f42f7f73e48e726e4799cd4
Paulo16Rubens <prubens42@gmail.com>
2017-06-01 01:50:54 +0100

    Create exemplo15.php

---
+++ exemplo15.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<?php
+function mais5($numero){
+$numero += 5;
+}
+echo"<h1>Passagem de Parâmetro</h1>";
+$a=3;
+mais5($a);
+echo"Valor da Variável a = $a <br>";
+?>
+</body>
+</html>
